e027af09c4272a5c6869bb58ddb7353538b262ea
Hesham Salman <hesham8@gmail.com>
2015-10-20 13:33:49 -0400

    Update MixTableViewCell.swift

--- Coding_Challenge/Coding_Challenge/MixTableViewCell.swift
+++ Coding_Challenge/Coding_Challenge/MixTableViewCell.swift
@@ -21,17 +21,6 @@ class MixTableViewCell: UITableViewCell {
 
     var mix: Mix?
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
     func loadItem(mix mix: Mix?) {
         if let m = mix {
             self.mix = m
