9d1fbaff996b96799124bc14b2b03e3b2881626f
Hesham Salman <hesham8@gmail.com>
2015-10-27 16:51:21 -0400

    Update ImageLoader.swift

--- Coding_Challenge/Coding_Challenge/ImageLoader.swift
+++ Coding_Challenge/Coding_Challenge/ImageLoader.swift
@@ -12,21 +12,16 @@ class ImageLoader {
     
     var cache = NSCache()
     
-    class var sharedLoader : ImageLoader {
-        struct Static {
-            static let instance : ImageLoader = ImageLoader()
-        }
-        return Static.instance
-    }
+    let sharedInstance = ImageLoader()
     
     func imageForUrl(urlString: String, completionHandler:(image: UIImage?, url: String) -> ()) {
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), {()in
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), { () in
             let data: NSData? = self.cache.objectForKey(urlString) as? NSData
             
             // Return cached image, if image is cached
             if let cachedData = data {
                 let image = UIImage(data: cachedData)
-                dispatch_async(dispatch_get_main_queue(), {() in
+                dispatch_async(dispatch_get_main_queue(), { () in
                     completionHandler(image: image, url: urlString)
                 })
                 return
@@ -44,7 +39,7 @@ class ImageLoader {
                 if data != nil {
                     let image = UIImage(data: data!)
                     self.cache.setObject(data!, forKey: urlString)
-                    dispatch_async(dispatch_get_main_queue(), {() in
+                    dispatch_async(dispatch_get_main_queue(), { () in
                         completionHandler(image: image, url: urlString)
                     })
                     return
